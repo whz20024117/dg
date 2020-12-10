97a4bcfb5d0b80734193e107f3281b805d3c8a5d
Marek Chalupa <mchqwerty@gmail.com>
2020-12-10 22:06:14 +0100

    Fix cutting off diverging
    We must handle also calls in blocks, not only callers.
    + properly remove the blocks.

--- tools/llvm-slicer-preprocess.cpp
+++ tools/llvm-slicer-preprocess.cpp
@@ -37,6 +37,16 @@ using namespace llvm;
 namespace dg {
 namespace llvmdg {
 
+template <typename C>
+inline bool hasRelevantPredecessor(llvm::BasicBlock *B,
+                                   const C& relevant) {
+    for (auto *p : llvm::predecessors(B)) {
+        if (relevant.count(p) > 0)
+            return true;
+    }
+    return false;
+}
+
 // FIXME: refactor
 // FIXME: configurable entry
 bool cutoffDivergingBranches(Module& M, const std::string& entry,
@@ -65,8 +75,28 @@ bool cutoffDivergingBranches(Module& M, const std::string& entry,
         if (!I) {
             continue;
         }
-        if (visited.insert(I->getParent()).second) {
-            queue.push(I->getParent());
+        auto *blk = I->getParent();
+        // add the block of slicing criteria
+        if (visited.insert(blk).second) {
+            queue.push(blk);
+        }
+
+        // add the callers of calls that reach this SC in this block
+        for (auto& blkI : *blk) {
+            if (&blkI == I)
+                break;
+            auto *blkCall = llvm::dyn_cast<llvm::CallInst>(&blkI);
+            if (!blkCall)
+                continue;
+            for (auto *fun : CG.getCalledFunctions(blkCall)) {
+                for (auto& funBlk : *fun) {
+                    if (llvm::isa<llvm::ReturnInst>(funBlk.getTerminator())) {
+                        if (visited.insert(const_cast<llvm::BasicBlock*>(&funBlk)).second) {
+                            queue.push(const_cast<llvm::BasicBlock*>(&funBlk));
+                        }
+                    }
+                }
+          }
         }
     }
 
@@ -78,6 +108,22 @@ bool cutoffDivergingBranches(Module& M, const std::string& entry,
 
         // paths from this block go to the slicing criteria
         relevant.insert(cur);
+
+        // queue the blocks from calls in current block
+        for (auto& blkI : *cur) {
+            auto *blkCall = llvm::dyn_cast<llvm::CallInst>(&blkI);
+            if (!blkCall)
+                continue;
+            for (auto *fun : CG.getCalledFunctions(blkCall)) {
+                for (auto& funBlk : *fun) {
+                    if (llvm::isa<llvm::ReturnInst>(funBlk.getTerminator())) {
+                        if (visited.insert(const_cast<llvm::BasicBlock*>(&funBlk)).second) {
+                            queue.push(const_cast<llvm::BasicBlock*>(&funBlk));
+                        }
+                    }
+                }
+          }
+        }
 
         if ((pred_begin(cur) == pred_end(cur))) {
             // pop-up from call
@@ -114,17 +160,29 @@ bool cutoffDivergingBranches(Module& M, const std::string& entry,
     exitF->addFnAttr(Attribute::NoReturn);
 
     for (auto& F : M) {
-      for (auto& B : F) {
-        if (relevant.count(&B) == 0) {
-          auto new_CI = CallInst::Create(exitF, {ConstantInt::get(argTy, 0)});
-          auto *point = B.getFirstNonPHI();
-          //CloneMetadata(point, new_CI);
-          new_CI->insertBefore(point);
+        std::vector<llvm::BasicBlock *> irrelevant;
+        for (auto& B : F) {
+          if (relevant.count(&B) == 0) {
+              irrelevant.push_back(&B);
+          }
         }
-      }
+        for (auto *B : irrelevant) {
+            // if this irrelevant block has predecessors in relevant,
+            // replace it with abort/exit
+            if (hasRelevantPredecessor(B, relevant)) {
+                auto *newB = BasicBlock::Create(Ctx, "diverge", &F);
+                CallInst::Create(exitF, {ConstantInt::get(argTy, 0)}, "", newB);
+                //CloneMetadata(point, new_CI);
+                new UnreachableInst(Ctx, newB);
+                // we cannot do the replacement here, we would break the iterator
+                B->replaceAllUsesWith(newB);
+            } // else just erase it
+            B->dropAllReferences();
+            B->eraseFromParent();
+        }
     }
 
-  return true;
+    return true;
 }
 
 } // namespace llvmdg
